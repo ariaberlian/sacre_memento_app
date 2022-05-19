be438c977b2fc35867a1b8033178a5dbc7237c33
ariaberlian <aria.berlian11@gmail.com>
2022-05-19 21:26:03 +0700

    fix: clear checkedlist

--- lib/blocs/select_mode.dart
+++ lib/blocs/select_mode.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sacre_memento_app/ui/home.dart';
 
 class SelectModeCubit extends Cubit<bool> {
   SelectModeCubit() : super(false);
 
   void selectMode(bool yes) {
-    return yes ? emit(true) : emit(false);
+    if (yes) {
+      return emit(true);
+    } else {
+      for (var i = 0; i < Home.count; i++) {
+        Home.isChecked[i] = false;
+      }
+      return emit(false);
+    }
   }
 }
-
